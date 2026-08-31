2342231416ff8ffc3c3036813fe11cf2f31dcfe4
Josh Stagg <josh@stagg.me>
2026-08-30 18:32:42 -0700

    Copy over RetainedValuesStoreProvider from Circuit

--- retainx/build.gradle.kts
+++ retainx/build.gradle.kts
@@ -1,3 +1,7 @@
+import com.android.build.api.withAndroid
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 // Copyright (C) 2026 Josh Stagg
 // SPDX-License-Identifier: MIT
 plugins {
@@ -35,32 +39,67 @@ kotlin {
     }
   }
 
-  // iOS Targets
+  // Native Desktop Targets
+  linuxArm64()
+  linuxX64()
+  mingwX64()
+
+  // Apple Targets
   iosArm64()
   iosSimulatorArm64()
-
-  // macOS Target
+  watchosArm32()
+  watchosArm64()
+  watchosSimulatorArm64()
+  tvosArm64()
+  tvosSimulatorArm64()
   macosArm64()
 
   // Web / Wasm Targets
-  @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
+  @OptIn(ExperimentalWasmDsl::class)
   wasmJs {
     browser()
+    binaries.executable()
   }
   js {
     browser()
+    binaries.executable()
   }
 
   // Apply default source set hierarchy
-  applyDefaultHierarchyTemplate()
+  @OptIn(ExperimentalKotlinGradlePluginApi::class)
+  applyDefaultHierarchyTemplate {
+    common {
+      group("shared") {
+        withAndroid()
+        withJvm()
+      }
+    }
+  }
+
+  targets.configureEach {
+    compilations.configureEach {
+      compileTaskProvider.configure {
+        compilerOptions { freeCompilerArgs.add("-Xexpect-actual-classes") }
+      }
+    }
+  }
 
   sourceSets {
     commonMain.dependencies {
-      implementation(compose.runtime)
-      implementation(compose.foundation)
-      implementation(compose.ui)
-      implementation(libs.kotlinx.coroutines.core)
+      api(libs.compose.runtime.retain)
     }
+
+    val sharedMain =
+      maybeCreate("sharedMain").apply {
+        // ViewModel doesn't have artifacts for linux, tvOS, watchOS, or Windows
+        dependencies {
+          implementation(libs.lifecycle.runtime.compose)
+          implementation(libs.lifecycle.viewModel.compose)
+        }
+      }
+    iosMain { dependsOn(sharedMain) }
+    macosMain { dependsOn(sharedMain) }
+    webMain { dependsOn(sharedMain) }
 
     commonTest.dependencies {
       implementation(libs.kotlin.test)
---
+++ retainx/src/tvosMain/kotlin/me/stagg/retainx/RetainedValuesStoreProvider.tvos.kt
@@ -0,0 +1,8 @@
+// Copyright (C) 2026 Slack Technologies, LLC
+// SPDX-License-Identifier: Apache-2.0
+package me.stagg.retainx
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal actual fun automaticRetainedValuesStoreOwner(): RetainedValuesStoreOwner? = null
